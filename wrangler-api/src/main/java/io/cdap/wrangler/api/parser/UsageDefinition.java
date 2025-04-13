7f3fced26e8be7aa7ba270fcaced65eccacab289
suhana <suhanakaura01@gmail.com>
2025-04-13 15:07:00 +0530

    Updated the usage definition and tokenType to support specifying these new token types as valid directive arguments.

--- wrangler-api/src/main/java/io/cdap/wrangler/api/parser/UsageDefinition.java
+++ wrangler-api/src/main/java/io/cdap/wrangler/api/parser/UsageDefinition.java
@@ -126,6 +126,10 @@ public final class UsageDefinition implements Serializable {
           sb.append("prop:{key:value,[key:value]*");
         } else if (token.type().equals(TokenType.RANGES)) {
           sb.append("start:end=[bool|text|numeric][,start:end=[bool|text|numeric]*");
+        } else if (token.type().equals(TokenType.BYTE_SIZE)) {
+          sb.append(token.name()).append(" (byte size, e.g., 10KB, 1.5MB)"); // For BYTE_SIZE tokens
+        } else if (token.type().equals(TokenType.TIME_DURATION)) {
+          sb.append(token.name()).append(" (time, e.g., 150ms, 2s)"); // For TIME_DURATION tokens
         }
       }
 
